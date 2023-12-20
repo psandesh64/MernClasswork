57df12c847e859302528d4bf1742aae272d1fb42
psandesh64 <psandesh64@gmail.com>
2023-12-20 19:18:57 +0545

    first commit

--- classReactProject/src/App.jsx
+++ classReactProject/src/App.jsx
@@ -10,13 +10,29 @@ const App = () => {
     'All the world\'s a stage, and all the men and women merely players. ',
   ]
   const [selected,setSelected] = useState(0);
-  const handleNextQuote=() => setSelected(selected<quotes.length-1 ? selected+1 : 0)
+  const [votes, setVotes] = useState([0,0,0]);
+
+  const handleNextQuote = () => setSelected(selected<quotes.length-1 ? selected+1 : 0);
+
+  // Update handleVote to increment the vote for the selected quote
+  const handleVote = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
+
   return (
     <div>
       <p>{quotes[selected]}</p>
+      <p>Votes: {votes[selected]}</p>
+      <button onClick={handleVote}>Vote</button>
       <button onClick={handleNextQuote}>Next quote</button>
     </div>
-  )
+  );
 }
 
 export default App
+
+/*
+const [votes, setVotes] = useState(Array(quotes.length).fill(0));
+*/
