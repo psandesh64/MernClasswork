277a327dff94bf03edf4458ab266c30cb9418b90
psandesh64 <psandesh64@gmail.com>
2023-12-20 12:20:25 +0545

    second commit

--- classReactProject/src/App.jsx
+++ classReactProject/src/App.jsx
@@ -3,6 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 
+const App = () => {
+  const quotes = [
+    'A rose by other name would smell as sweet.',
+    'All that glitters is not gold.',
+    'All the world\'s a stage, and all the men and women merely players. ',
+  ]
+  const [selected,setSelected] = useState(0);
 
+  return (
+    <div>
+      {quotes[selected]}
+    </div>
+  )
+}
 
 export default App
