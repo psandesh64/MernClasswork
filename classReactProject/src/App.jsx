8838c840abeb97e07dbb7018cdb290222faa1dcc
psandesh64 <psandesh64@gmail.com>
2023-12-21 08:01:23 +0545

    first commit

--- classReactProject/src/App.jsx
+++ classReactProject/src/App.jsx
@@ -1,38 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
+import { useState } from "react";
 
 const App = () => {
-  const quotes = [
-    'A rose by other name would smell as sweet.',
-    'All that glitters is not gold.',
-    'All the world\'s a stage, and all the men and women merely players. ',
-  ]
-  const [selected,setSelected] = useState(0);
-  const [votes, setVotes] = useState([0,0,0]);
 
-  const handleNextQuote = () => setSelected(selected<quotes.length-1 ? selected+1 : 0);
+  let quotes = [
+    "Embrace the journey, for within the twists and turns, you discover the true colors of your soul.",
 
-  // Update handleVote to increment the vote for the selected quote
-  const handleVote = () => {
-    const newVotes = [...votes];
-    newVotes[selected] += 1;
-    setVotes(newVotes);
-  };
+    "In the symphony of life, let resilience be the melody that carries you through every note of challenge.",
+
+    "The canvas of your dreams awaits the brushstrokes of your determination. Paint a masterpiece.",
+
+    "Wisdom whispers in the silence between words, and growth thrives in the spaces between comfort and chaos.",
+
+    "Cherish the power of now, for it is the seed from which the tree of your future will bloom."
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [highestIndex, setHighestIndex] = useState(0);
+  const [points, setPoints] = useState([0,0,0,0,0]);
+
+  const nextQuote = () => {
+    if(selected === quotes.length -1) {
+      setSelected(0);
+    } else {
+      setSelected(selected+1)
+    }
+  }
+
+  const checkHighestVote = () => {
+    console.log("checking")
+    // loop points and check which index has highest
+    // set highest index to a seperate state variable
+  }
+
+  const vote = () => {
+    const copy = [...points];
+    copy[selected] += 1;
+    checkHighestVote();
+    setPoints(copy);
+  }
 
   return (
-    <div>
+    <>
+      <h1>Quote of the day</h1>
       <p>{quotes[selected]}</p>
-      <p>Votes: {votes[selected]}</p>
-      <button onClick={handleVote}>Vote</button>
-      <button onClick={handleNextQuote}>Next quote</button>
-    </div>
-  );
+      <button onClick={vote}>Vote</button>
+      <button onClick={nextQuote}>Next quote</button>
+      <h1>Quotes</h1>
+      <p>{quotes[selected]}</p>
+      <p>has {points[selected]} votes.</p>
+      <button onClick={vote}>Vote</button>
+      <button onClick={nextQuote}>Next quote</button>
+    </>
+  )
 }
-
 export default App
-
-/*
-const [votes, setVotes] = useState(Array(quotes.length).fill(0));
-*/
